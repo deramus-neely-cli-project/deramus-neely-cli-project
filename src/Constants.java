808a2089002a73b6892aa39c06910d50d00d61a2
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-01-17 22:34:33 -0600

    added a constants class to hold all the constants, need to fix

---
+++ src/Constants.java
@@ -0,0 +1,20 @@
+final public class Constants {
+  public static final String welcomeMessage = "Welcome to Contacts Manager";
+  public static final String addContact = "1. Add a new contact";
+  public static final String searchContact = "2. Search for a contact";
+  public static final String deleteContact = "3. Delete a contact";
+  public static final String exit = "4. Exit";
+  public static final String enterChoice = "Enter your choice: ";
+  public static final String addContactChoice = "You have chosen to add a new contact: ";
+  public static final String nameSearch = "Enter the name of the contact: ";
+  public static final String enterNumber = "Enter the phone number of the contact: ";
+  public static final String contactSearch = "Enter the name of the contact to search for: ";
+  public static final String noResults = "NO RESULTS FOUND!";
+  public static final String contactList = "Here are your contacts: ";
+  public static final String deleteChoice = "Enter the name of the contact to delete: ";
+  public static final String exiting = "Exiting...";
+  public static final String wrongChoice = "Wrong choice";
+  public static final String fileName = "Contacts.txt";
+}
+
+// TODO: Try and import this to src\App.java and use the constants there
