98920931de9785b01c4dea274a17824e3d314731
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-01-17 23:27:20 -0600

    formatted spacing

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -2,39 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class FileHandler {
-    private final String fileName;
-    public FileHandler(String fileName) {
-        this.fileName = fileName;
-    }
+  private final String fileName;
 
-    public List<Contact> readContacts() {
-        List<Contact> contacts = new ArrayList<>();
-        File file = new File(fileName);
-        if (file.exists()) {
-            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-                String line;
-                while ((line = br.readLine()) != null) {
-                    String[] contactDetails = line.split(" ");
-                    String name = contactDetails[0];
-                    String phoneNumber = contactDetails[1];
-                    contacts.add(new Contact(name, phoneNumber));
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return contacts;
-    }
+  public FileHandler(String fileName) {
+    this.fileName = fileName;
+  }
 
-    public void writeContacts(List<Contact> contacts) {
-        File file = new File(fileName);
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
-            for (Contact contact : contacts) {
-                bw.write(contact.getName() + " " + contact.getPhoneNumber());
-                bw.newLine();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+  public List<Contact> readContacts() {
+    List<Contact> contacts = new ArrayList<>();
+    File file = new File(fileName);
+    if (file.exists()) {
+      try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+        String line;
+        while ((line = br.readLine()) != null) {
+          String[] contactDetails = line.split(" ");
+          String name = contactDetails[0];
+          String phoneNumber = contactDetails[1];
+          contacts.add(new Contact(name, phoneNumber));
         }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
+    return contacts;
+  }
+
+  public void writeContacts(List<Contact> contacts) {
+    File file = new File(fileName);
+    try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
+      for (Contact contact : contacts) {
+        bw.write(contact.getName() + " " + contact.getPhoneNumber());
+        bw.newLine();
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
